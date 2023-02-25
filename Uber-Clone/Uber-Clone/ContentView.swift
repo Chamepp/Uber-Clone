d941b1b14051495ccdb2dbb15370bf7948370168
System Administrator <ashkanebtekari@gmail.com>
2023-02-25 17:15:28 +0330

    :star: started the project

---
+++ Uber-Clone/Uber-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Uber-Clone
+//
+//  Created by Ashkan Ebtekari on 2/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Uber-Clone/Uber-Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber-Clone
+//
+//  Created by Ashkan Ebtekari on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
