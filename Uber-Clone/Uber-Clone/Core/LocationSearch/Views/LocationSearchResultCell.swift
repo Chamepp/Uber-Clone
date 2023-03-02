1de0d7f6ae94955b7b180db27ff6d9c25738570d
System Administrator <ashkanebtekari@gmail.com>
2023-03-02 18:31:46 +0330

    created the search result cell component and added it to the home view

---
+++ Uber-Clone/Uber-Clone/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -0,0 +1,38 @@
+//
+//  LocationSearchResultCell.swift
+//  Uber-Clone
+//
+//  Created by Ashkan Ebtekari on 3/2/23.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "mappin.circle.fill")
+                .resizable()
+                .foregroundColor(.blue)
+                .accentColor(.white)
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Starbucks Coffee")
+                    .font(.body)
+                Text("123 Main St, Cupertino CA")
+                    .font(.system(size: 15))
+                    .foregroundColor(.gray)
+                Divider()
+            }
+            .padding(.leading, 8)
+            .padding(.vertical, 8)
+        }
+        .padding(.leading)
+    }
+}
+
+struct LocationSearchResultCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchResultCell()
+    }
+}
---
+++ Uber-Clone/Uber-Clone/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,60 @@
+//
+//  LocationSearchView.swift
+//  Uber-Clone
+//
+//  Created by Ashkan Ebtekari on 3/2/23.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    
+    @State private var start_location_text = ""
+    @State private var destination_location_text = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                VStack {
+                    TextField("Current location", text: $start_location_text)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where to?", text: $destination_location_text)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        LocationSearchResultCell()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
