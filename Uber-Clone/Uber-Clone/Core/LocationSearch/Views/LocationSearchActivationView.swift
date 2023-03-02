e219abe796e619eacd5d5b9c035074fcffe5921f
System Administrator <ashkanebtekari@gmail.com>
2023-03-02 15:21:12 +0330

    Organizing Groups

---
+++ Uber-Clone/Uber-Clone/Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,33 @@
+//
+//  LocationSearchActivationView.swift
+//  Uber-Clone
+//
+//  Created by Ashkan Ebtekari on 3/2/23.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 8, height: 8)
+                .padding(.horizontal)
+            Text("Where to ?")
+                .foregroundColor(Color(.darkGray))
+            
+            Spacer()
+                .frame(width: UIScreen.main.bounds.width - 64, height: 50)
+            Rectangle()
+                .fill(Color.white)
+                .shadow(color: .black, radius: 6)
+        }
+    }
+}
+
+struct LocationSearchActivationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivationView()
+    }
+}
