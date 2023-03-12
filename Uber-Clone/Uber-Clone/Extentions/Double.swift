8ef488c74cdda1da0a584704ce364dc3a03c87ee
System Administrator <ashkanebtekari@gmail.com>
2023-03-12 19:25:50 +0330

    completed the currency formatter extension

--- Uber-Clone/Uber-Clone/Extentions/Double.swift
+++ Uber-Clone/Uber-Clone/Extentions/Double.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Double {
+    private var currency_formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func to_currency() -> String {
+        return currency_formatter.string(for: self) ?? ""
+    }
+}
